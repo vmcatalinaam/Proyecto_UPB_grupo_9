35e06c7ef19fc004bcc4fdb4c9c985c43c0f2012
Vladimir Herrera <91296344+VladHeimdall@users.noreply.github.com>
2021-10-20 15:54:28 -0500

    código envió de datos

---
+++ Sprint3/regis.php
@@ -0,0 +1,10 @@
+<?php
+//CONECTAMOS CON LA BASE DE DATOS
+$connection = mysqli_connect("localhost", "root", "", "form") 
+or die("Error en coneccion");
+$consulta = "INSERT INTO form VALUES ('".$_POST["nombre"]."','".$_POST["edad"]."','".$_POST["contrasena"]."')";
+$resultado=mysqli_query($connection,$consulta) or die ("error query");
+mysqli_close($connection);
+echo 'El registro ha sido completado, para el usuario:'.$_POST["nombre"];
+header("Location:postregistro.html");//CON ESTO REDIRECCIONO EN PHP
+?>
